d5b2d36be3277f815feb00e457c67b9b9f2c0aec
Jorja Hung <jorjahung@gmail.com>
2014-02-15 09:51:56 +0000

    fixed peep class

--- app/models/peep.rb
+++ app/models/peep.rb
@@ -5,12 +5,15 @@ class Peep
 	# this makes the instances of this class DataMapper resources
 	include DataMapper::Resource
 
-
+	has 1, :user, :through => Resource
 
 	# this block describes what resources our model will have
 	property :id, Serial #Serial means that it will be auto-incremented for every record
 	property :message, Text
 	property :created_at, DateTime
+	property :poster_fullname, String
+	property :poster_username, String
 
 end
 
+# Jorja, maybe not cheat? <%= user.first(:id => peep.user).full_name %> (<%= user.first(:id => peep.user).username %>)
