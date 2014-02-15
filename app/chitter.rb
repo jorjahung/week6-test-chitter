778195a4393962330ef2498180ab7fecdd72f45c
Jorja Hung <jorjahung@gmail.com>
2014-02-15 09:57:29 +0000

    more tweaking

--- app/chitter.rb
+++ app/chitter.rb
@@ -10,11 +10,20 @@ set :session_secret, 'super secret'
 use Rack::Flash
 set :partial_template_engine, :erb
 
+# # models before data_mapper_setup:
 require_relative 'models/peep'
 require_relative 'models/user'
 
+# # controllers:
 require_relative 'controllers/application'
 require_relative 'controllers/peeps'
+require_relative 'controllers/sessions'
 require_relative 'controllers/users'
 
+
+require_relative 'helpers/application'
 require_relative 'data_mapper_setup'
+
+
+
+
