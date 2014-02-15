6e2a122510b6f20e216accfb8497f03205b3914b
Jorja Hung <jorjahung@gmail.com>
2014-02-15 21:06:34 +0000

    trying to make css work on Heroku

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,6 +1,7 @@
 env = ENV["RACK_ENV"] || "development"
-DataMapper.setup(:default, "postgres://localhost/chitter_#{env}")
-# For Heroku staging: DataMapper.setup(:default, "postgres://nxhwfanozypvmw:mt6G6iV8q_9w9OBmFjejnijEvh@ec2-107-20-245-187.compute-1.amazonaws.com:5432/da8kn3h231bdq6")
+# DataMapper.setup(:default, "postgres://localhost/chitter_#{env}")
+# For Heroku staging: 
+DataMapper.setup(:default, "postgres://nxhwfanozypvmw:mt6G6iV8q_9w9OBmFjejnijEvh@ec2-107-20-245-187.compute-1.amazonaws.com:5432/da8kn3h231bdq6")
 
 require './app/chitter'
 
